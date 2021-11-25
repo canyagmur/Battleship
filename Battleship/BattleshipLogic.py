1f31fe8d6e0db074232175b0a80e3b8f955be47d
canyagmur <can.yagmur@ozu.edu.tr>
2021-11-25 03:21:21 +0300

    Game logic is finished.

--- Battleship/BattleshipLogic.py
+++ Battleship/BattleshipLogic.py
@@ -1,44 +1,31 @@
 from Fleet import Fleet
 import numpy as np
-from prompt_toolkit import print_formatted_text, HTML,ANSI
+from prompt_toolkit import print_formatted_text, HTML, ANSI
 from prompt_toolkit.styles import Style
 from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.completion import WordCompleter
 import os
 from ShipText import ShipText
-
+import traceback
 
 
 class GridMaker:
     LETTER_RANGE = 'abcdefghijklmnopqrstuvwxyz'.upper()  # ALPHABET
-    PLACE_HOLDER = "-"
-    GRID_YOU = None
-    GRID_OPPONENT = None
+    all_locations = []
 
-    def __init__(self, game_size, username1,username2,space_size=4):
-
-        """Game size should be between 10-26"""
-        limited_value = min(game_size, 26)
-        self.game_size = max(limited_value, 10)
-
-        self.GRID_YOU = np.empty((self.game_size, self.game_size), dtype=str)
-        self.GRID_YOU[:] = self.PLACE_HOLDER
-        self.username1 = username1
-
-
-        self.GRID_OPPONENT = np.empty((self.game_size, self.game_size), dtype=str)
-        self.GRID_OPPONENT[:] = self.PLACE_HOLDER
-        self.username2 = username2
-
+    def __init__(self, game_size, username1, username2, space_size, PLACE_HOLDER):
+        self.game_size = game_size
         self.space_size = space_size
+        self.username1 = username1
+        self.username2 = username2
+        self.PLACE_HOLDER = PLACE_HOLDER
 
-        self.all_locations = []
         for letter in self.LETTER_RANGE[0:self.game_size]:
-            for number in range(1,self.game_size+1):
-                self.all_locations.append(letter+str(number))
+            for number in range(1, self.game_size + 1):
+                self.all_locations.append(letter + str(number))
 
-    def display_grids(self):
+    def display_grids(self, grid_you, grid_opponent_r):
         grid_size = self.game_size + 1
         for i in range(grid_size):
             for j in range(grid_size):
@@ -46,78 +33,74 @@ class GridMaker:
                     print_formatted_text(" " * (self.space_size + 1), end="")
                 elif i == 0 and j != 0:
                     mytext = self.LETTER_RANGE[j - 1]
-                    print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(mytext),end=" ") #print(self.LETTER_RANGE[j - 1], end=" ")
+                    print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(mytext),
+                                         end=" ")  # print(self.LETTER_RANGE[j - 1], end=" ")
                 elif i != 0 and j == 0:
                     if i >= 10:
-                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * (self.space_size - 1)) #print_formatted_text(i, end=" " * (self.space_size - 1))
+                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * (
+                                    self.space_size - 1))  # print_formatted_text(i, end=" " * (self.space_size - 1))
                     else:
-                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * self.space_size)#print_formatted_text(i, end=" " * self.space_size)
+                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i),
+                                             end=" " * self.space_size)  # print_formatted_text(i, end=" " * self.space_size)
                 else:
-                    mytext=self.GRID_YOU[i - 1, j - 1]
-                    print_formatted_text(HTML('<b><ansigreen>{}</ansigreen></b>').format(mytext), end=" ")
+                    mytext = grid_you[i - 1, j - 1]
+                    print_formatted_text(HTML('<b><ansiwhite>{}</ansiwhite></b>').format(mytext), end=" ")
             print_formatted_text(" " * 2 * self.space_size, end="")
 
             for j in range(grid_size):
                 if i == 0 and j == 0:
-                    #print(" " * (self.space_size + 1), end="")
+                    # print(" " * (self.space_size + 1), end="")
                     mytext = " " * (self.space_size + 1)
-                    print_formatted_text(mytext,end="")
+                    print_formatted_text(mytext, end="")
                 elif i == 0 and j != 0:
                     mytext = self.LETTER_RANGE[j - 1]
                     print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(mytext), end=" ")
-                    #print(self.LETTER_RANGE[j - 1], end=" ")
+                    # print(self.LETTER_RANGE[j - 1], end=" ")
                 elif i != 0 and j == 0:
                     if i >= 10:
-                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * (self.space_size - 1)) #print_formatted_text(i, end=" " * (self.space_size - 1))
+                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * (
+                                    self.space_size - 1))  # print_formatted_text(i, end=" " * (self.space_size - 1))
                     else:
-                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i), end=" " * self.space_size)#print_formatted_text(i, end=" " * self.space_size)
+                        print_formatted_text(HTML('<b><ansired>{}</ansired></b>').format(i),
+                                             end=" " * self.space_size)  # print_formatted_text(i, end=" " * self.space_size)
                 else:
-                    mytext=self.GRID_OPPONENT[i - 1, j - 1]
-                    print_formatted_text(HTML('<b><ansigreen>{}</ansigreen></b>').format(mytext), end=" ")
-                    #print(self.GRID_OPPONENT[i - 1, j - 1], end=" ")
+                    mytext = grid_opponent_r[i - 1, j - 1]
+                    print_formatted_text(HTML('<b><ansiwhite>{}</ansiwhite></b>').format(mytext), end=" ")
+                    # print(self.GRID_OPPONENT[i - 1, j - 1], end=" ")
             print_formatted_text("")  # NEW LINE
             if i == 0: print_formatted_text("")
 
-    def deploy_fleet(self,user):
-        username=""
-        if user==1:
-            username=self.username1
-            USER = self.GRID_YOU
-        else:
-            username = self.username2
-            USER = self.GRID_OPPONENT
-
-
-
+    def deploy_fleet(self, username, GRID_YOU, GRID_OPPONENT_R):
 
         print_formatted_text("\n" * 3)
-        print_formatted_text(HTML('<ansiwhite><i>---------------------DEPLOY YOUR FLEET--------------------</i></ansiwhite>'))
+        print_formatted_text(
+            HTML('<ansiwhite><i>---------------------DEPLOY YOUR FLEET--------------------</i></ansiwhite>'))
         print_formatted_text("\n" * 2)
         fleet_enums = [i for i in Fleet]
 
         possible_locs = self.all_locations.copy()
 
         for ship_enum in fleet_enums:
-            head, tail = self.get_locations(ship_enum,possible_locs,username)
+            head, tail = self.get_locations(ship_enum, possible_locs, username)
             while True:
-                if self.is_block_empty(head, tail,USER):
-                    possible_locs=self.put_ship(head, tail, ship_enum,possible_locs,USER)
+                if self.is_block_empty(head, tail, GRID_YOU):
+                    possible_locs = self.put_ship(head, tail, ship_enum, possible_locs, GRID_YOU)
                     break
                 else:
                     print_formatted_text(HTML("<ansired>Overlapping blocks for ships detected!</ansired>"))
                     print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
-                    head, tail = self.get_locations(ship_enum,possible_locs,username)
+                    head, tail = self.get_locations(ship_enum, possible_locs, username)
                     continue
-        print_formatted_text("\n"*3)
+        print_formatted_text("\n" * 3)
         print_formatted_text(HTML('<ansicyan>The fleet is ready for your command!</ansicyan>'))
 
-        self.display_grids()
+        self.display_grids(GRID_YOU, GRID_OPPONENT_R)
+        return GRID_YOU
 
-    def is_grid_valid_and_empty(self,loc,available_locs):
+    def is_grid_valid_and_empty(self, loc, available_locs):
         return loc in available_locs
 
-
-    def is_block_empty(self, head, tail,USER):
+    def is_block_empty(self, head, tail, USER):
         x_h, y_h = head
         x_t, y_t = tail
         start_x, end_x = tuple(sorted([x_t, x_h]))
@@ -125,31 +108,30 @@ class GridMaker:
         filter_list = USER[start_x:end_x + 1, start_y:end_y + 1] == self.PLACE_HOLDER
         return False not in filter_list
 
-    def put_ship(self, head, tail, fleet_type,possible_locs,USER):
-        print_formatted_text("\n"*2)
+    def put_ship(self, head, tail, fleet_type, possible_locs, USER):
+        print_formatted_text("\n" * 2)
         text = "\t\t\t{} is deployed !".format(fleet_type.name)
         print_formatted_text(HTML("<ansicyan>{}</ansicyan>").format(text))
-        print_formatted_text("\n"*2)
+        print_formatted_text("\n" * 2)
         x_h, y_h = head
         x_t, y_t = tail
         start_x, end_x = tuple(sorted([x_t, x_h]))
         start_y, end_y = tuple(sorted([y_t, y_h]))
         USER[start_x:end_x + 1, start_y:end_y + 1] = fleet_type.name[0]
-        for i in range(start_x,end_x+1):
-            for j in range(start_y,end_y+1):
-                loc = self.LETTER_RANGE[j]+str(i+1)
+        for i in range(start_x, end_x + 1):
+            for j in range(start_y, end_y + 1):
+                loc = self.LETTER_RANGE[j] + str(i + 1)
                 possible_locs.remove(loc)
-        return  possible_locs
+        return possible_locs
 
-
-    def get_locations(self, fleet_type,available_loc_list,username):
+    def get_locations(self, fleet_type, available_loc_list, username):
         while True:
-            if fleet_type.name=="CARRIER" : text = ShipText.CARRIER.value
+            if fleet_type.name == "CARRIER": text = ShipText.CARRIER.value
             if fleet_type.name == "BATTLESHIP": text = ShipText.BATTLESHIP.value
             if fleet_type.name == "SUBMARINE": text = ShipText.SUBMARINE.value
             if fleet_type.name == "DESTROYER": text = ShipText.DESTROYER.value
 
-            #text ="DEPLOY YOUR {} (LENGTH {})".format(fleet_type.name, fleet_type.value)
+            # text ="DEPLOY YOUR {} (LENGTH {})".format(fleet_type.name, fleet_type.value)
             print_formatted_text(HTML("<ansigreen>{}</ansigreen>").format(text))
             try:
 
@@ -173,36 +155,38 @@ class GridMaker:
                     ('class:pound', '# '),
                 ]
 
-
                 loc_completer = WordCompleter(available_loc_list)  # autocompleter
 
                 print_formatted_text(HTML('<ansiyellow>\tPLEASE SELECT THE LOCATION OF THE HEAD : </ansiyellow>'))
-                head = prompt(message, style=style,completer=loc_completer)
+                head = prompt(message, style=style, completer=loc_completer)
 
-                other_locs =[]
-                if self.is_grid_valid_and_empty(head,available_loc_list):
+                other_locs = []
+                if self.is_grid_valid_and_empty(head, available_loc_list):
                     letter = head[0]
                     number = int(head[1:])
                     ship_size = fleet_type.value
 
-                    letter_1 = chr(ord(letter)-ship_size+1) #search below
-                    letter_2 = chr(ord(letter)+ship_size-1) #search above
+                    letter_1 = chr(ord(letter) - ship_size + 1)  # search below
+                    letter_2 = chr(ord(letter) + ship_size - 1)  # search above
 
-                    if ord(letter_1) >= ord("A") and self.is_grid_valid_and_empty((letter_1+str(number)),available_loc_list):
-                        other_locs.append(letter_1+str(number))
-                    if ord(letter_2) <= ord(self.LETTER_RANGE[self.game_size-1]) and self.is_grid_valid_and_empty((letter_2+str(number)),available_loc_list):
-                        other_locs.append(letter_2+str(number))
+                    if ord(letter_1) >= ord("A") and self.is_grid_valid_and_empty((letter_1 + str(number)),
+                                                                                  available_loc_list):
+                        other_locs.append(letter_1 + str(number))
+                    if ord(letter_2) <= ord(self.LETTER_RANGE[self.game_size - 1]) and self.is_grid_valid_and_empty(
+                            (letter_2 + str(number)), available_loc_list):
+                        other_locs.append(letter_2 + str(number))
 
-                    number_1 = number-ship_size+1 #search below
-                    number_2 = number+ship_size-1 #search above
+                    number_1 = number - ship_size + 1  # search below
+                    number_2 = number + ship_size - 1  # search above
 
-                    if number_1 >= 1 and self.is_grid_valid_and_empty((letter+str(number_1)),available_loc_list)  :
-                        other_locs.append(letter+str(number_1))
+                    if number_1 >= 1 and self.is_grid_valid_and_empty((letter + str(number_1)), available_loc_list):
+                        other_locs.append(letter + str(number_1))
 
-                    if number_2 <= self.game_size and self.is_grid_valid_and_empty((letter+str(number_2)),available_loc_list):
-                        other_locs.append(letter+str(number_2))
+                    if number_2 <= self.game_size and self.is_grid_valid_and_empty((letter + str(number_2)),
+                                                                                   available_loc_list):
+                        other_locs.append(letter + str(number_2))
 
-                    #other_locs = [x for x in available_loc_list.copy() if x.startswith(head[0]) or x.endswith(head[1:])]
+                    # other_locs = [x for x in available_loc_list.copy() if x.startswith(head[0]) or x.endswith(head[1:])]
                 else:
                     print_formatted_text(HTML('<ansired>Mismatched location</ansired>'))
                     print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
@@ -211,7 +195,7 @@ class GridMaker:
                 loc_completer = WordCompleter(other_locs)
 
                 print_formatted_text(HTML('<ansiyellow>\tPLEASE SELECT THE LOCATION OF THE TAIL : </ansiyellow>'))
-                tail = prompt(message, style=style,completer=loc_completer)
+                tail = prompt(message, style=style, completer=loc_completer)
 
                 if tail in available_loc_list:
                     head = head.strip()
@@ -221,7 +205,6 @@ class GridMaker:
                     print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
                     continue
                 "whitespaces will be ignored"
-
 
                 if (head in available_loc_list) and (tail in available_loc_list):
 
@@ -236,7 +219,7 @@ class GridMaker:
 
                             return head, tail
 
-                        text ="\nLength of the {} must be {}".format(fleet_type.name, fleet_type.value)
+                        text = "\nLength of the {} must be {}".format(fleet_type.name, fleet_type.value)
                         print_formatted_text(HTML('<ansired>{}</ansired>'.format(text)))
                         print_formatted_text(HTML('<ansired>\nPlease try again...</ansired>'))
                         continue
@@ -246,21 +229,15 @@ class GridMaker:
                         print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
                         continue
 
-                else: #unnecessary
+                else:  # unnecessary
                     print_formatted_text(HTML('<ansired>Mismatched location</ansired>'))
                     print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
 
             except Exception as e:
+                if e is None:
+                    print_formatted_text(HTML('<ansired>Input format is wrong!</ansired>'))
+                    print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
+                    continue
                 print_formatted_text(e)
-                print_formatted_text(HTML('<ansired>Input format is wrong!</ansired>'))
-                print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
-                continue
-
-
-
-
-
-
-
-
-
+                print(traceback.format_exc())  # print stack trace
+                break
--- Battleship/Game.py
+++ Battleship/Game.py
@@ -2,15 +2,54 @@ from BattleshipLogic import GridMaker
 from ShipText import ShipText
 import os
 import time
+from prompt_toolkit.formatted_text import FormattedText
+from prompt_toolkit import print_formatted_text, HTML, ANSI
+import numpy as np
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
+from prompt_toolkit.shortcuts import prompt
+import traceback
+
+
+
+
 
 
 class Game():
+    PLACE_HOLDER = "-"
+    GRID_YOU = None
+    GRID_OPPONENT = None
+    HIT_SYMBOL = "*"
+    MISS_SYMBOL ="X"
+    GRID_FLEET_NAMES = ['C','B','S','D']
 
-    def __init__(self,username1="User-1",username2="User-2"):
-        self.username1= username1
+    def __init__(self, username1="User-1", username2="User-2", game_size=10, space_size=4):
+        """Usernames"""
+        self.username1 = username1
         self.username2 = username2
-        self.grid_maker = GridMaker(game_size=20, space_size=4,username1=username1,username2=username2)
 
+        """Game size should be between 10-26"""
+        limited_value = min(game_size, 26)
+        self.game_size = max(limited_value, 10)
+
+        self.space_size = space_size
+
+        self.GRID_YOU = np.empty((self.game_size, self.game_size), dtype=str)
+        self.GRID_YOU[:] = self.PLACE_HOLDER
+        self.GRID_YOU_R = np.empty((self.game_size, self.game_size), dtype=str)
+        self.GRID_YOU_R[:] = self.PLACE_HOLDER
+
+        self.GRID_OPPONENT = np.empty((self.game_size, self.game_size), dtype=str)
+        self.GRID_OPPONENT[:] = self.PLACE_HOLDER
+        self.GRID_OPPONENT_R = np.empty((self.game_size, self.game_size), dtype=str)
+        self.GRID_OPPONENT_R[:] = self.PLACE_HOLDER
+
+        """Grid Maker Object"""
+        self.grid_maker = GridMaker(game_size=self.game_size, space_size=self.space_size, username1=username1,
+                                    username2=username2, PLACE_HOLDER=self.PLACE_HOLDER)
+
+        self.all_locs_you_r = GridMaker.all_locations.copy()
+        self.all_locs_opponent_r = GridMaker.all_locations.copy()
 
     def clear_console(self):
         command = 'clear'
@@ -19,20 +58,124 @@ class Game():
         os.system(command)
 
     def start_game(self):
+        """CLEAN CONSOLE AND WELCOME SCREEN"""
         self.clear_console()
-        print(ShipText.WELCOME.value)
+        print_formatted_text(HTML("<ansiwhite>{}</ansiwhite>").format(ShipText.WELCOME.value))
         time.sleep(3)
-        print("\n"*3)
+        print("\n" * 3)
+
+        self.grid_maker.display_grids(self.GRID_YOU, self.GRID_OPPONENT_R)
+        self.GRID_YOU = self.grid_maker.deploy_fleet(username=self.username1, GRID_YOU=self.GRID_YOU,
+                                                     GRID_OPPONENT_R=self.GRID_OPPONENT_R)
+        self.GRID_OPPONENT = self.grid_maker.deploy_fleet(username=self.username2, GRID_YOU=self.GRID_OPPONENT,
+                                                          GRID_OPPONENT_R=self.GRID_YOU_R)
+        self.clear_console()
+        print("User 1 Perspective : ")
+        self.grid_maker.display_grids(self.GRID_YOU, self.GRID_OPPONENT_R)
+        time.sleep(3)
+        print("User 2 Perspective : ")
+        self.grid_maker.display_grids(self.GRID_OPPONENT, self.GRID_YOU_R)
+        time.sleep(3)
 
 
-        self.grid_maker.display_grids()
-        self.grid_maker.deploy_fleet(user=1)
-        self.grid_maker.deploy_fleet(user=2)
+        print("\nGame is starting .....")
+        winner = None
+        while True:
+            print("your turn user1 :")
+            self.all_locs_opponent_r = self.make_guess(username=self.username1, opponent_grid_real=self.GRID_OPPONENT,
+                                                       opponent_grid_relative=self.GRID_OPPONENT_R,
+                                                       possible_locs_opponent_r=self.all_locs_opponent_r)
+            self.grid_maker.display_grids(self.GRID_YOU, self.GRID_OPPONENT_R)
+            time.sleep(1)
+            if self.is_game_finished(self.GRID_OPPONENT):
+                winner=self.username1.upper()
+                break
 
 
+            print("your turn user2 :")
+            self.all_locs_you_r = self.make_guess(username=self.username2, opponent_grid_real=self.GRID_YOU,
+                                                  opponent_grid_relative=self.GRID_YOU_R,
+                                                  possible_locs_opponent_r=self.all_locs_you_r)
+            self.grid_maker.display_grids(grid_you=self.GRID_OPPONENT, grid_opponent_r=self.GRID_YOU_R)
+            time.sleep(1)
+            if self.is_game_finished(self.GRID_YOU):
+                winner=self.username2.upper()
+                break
+        print("Winner is :",winner)
+        self.grid_maker.display_grids(grid_you=self.GRID_YOU,grid_opponent_r=self.GRID_OPPONENT) #EXPLICIT SHOW
 
-game = Game()
+    def is_game_finished(self,grid):
+        has_true = False
+        for char in self.GRID_FLEET_NAMES:
+            has_true = has_true or (char in grid)
+        return not has_true # if it has no characters then game is finished.
+
+    def convert_coordinate(self,location):
+        coor = int(location[1:]) - 1, GridMaker.LETTER_RANGE.index(location[0])
+
+        return coor
+
+    def make_guess(self,opponent_grid_real,opponent_grid_relative,username,possible_locs_opponent_r):
+        style = Style.from_dict({
+            # User input (default text).
+            '': '#ff0066',
+
+            # Prompt.
+            'username': 'ansicyan underline',
+            'at': '#00aa00',
+            'colon': '#0000aa',
+            'pound': '#00aa00',
+            'host': '#00ffff ',
+        })
+
+        message = [
+            ('class:username', username),
+            ('class:at', '@'),
+            ('class:host', 'localhost'),
+            ('class:colon', ':'),
+            ('class:pound', '# '),
+        ]
+        loc_completer = WordCompleter(possible_locs_opponent_r)
+
+        loc_copy = possible_locs_opponent_r.copy()
+
+        while True:
+            try:
+                print_formatted_text(HTML('<ansiyellow>\tPLEASE MAKE A GUESS </ansiyellow>'))
+                guess = prompt(message, style=style, completer=loc_completer)
+                if guess in GridMaker.all_locations :   #It checks whether the guess is wrong formatted or not.
+                     if guess in loc_copy:
+                        """From now on guess is valid"""
+                        x,y = self.convert_coordinate(guess)
+                        value = opponent_grid_real[x,y]
+                        loc_copy.remove(guess)
+                        if value in self.GRID_FLEET_NAMES: #hit condition
+                            opponent_grid_real[x,y] = self.HIT_SYMBOL
+                            opponent_grid_relative[x,y] = self.HIT_SYMBOL
+                            return  loc_copy
+                        else:
+                            opponent_grid_real[x,y] = self.MISS_SYMBOL
+                            opponent_grid_relative[x,y] = self.MISS_SYMBOL
+                            return loc_copy
+
+                     else:
+                        print_formatted_text(HTML('<ansired>This place already marked</ansired>'))
+                        print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
+                        continue
+                else:
+                    print_formatted_text(HTML('<ansired>Mismatched location</ansired>'))
+                    print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
+                    continue
+
+            except Exception as e:
+                if e is None:
+                    print_formatted_text(HTML('<ansired>Input format is wrong!</ansired>'))
+                    print_formatted_text(HTML('<ansired>Please try again...</ansired>'))
+                    continue
+                print_formatted_text(e)
+                print_formatted_text(traceback.format_exc())  # print stack trace
+                break
+
+
+game = Game(game_size=10)
 game.start_game()
-
-
-
